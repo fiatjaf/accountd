965ac1aef91717f833733c41be60346afa7623cf
fiatjaf <fiatjaf@gmail.com>
2018-03-15 18:22:30 +0000

    update to new accountd.xyz

--- accountd.go
+++ accountd.go
@@ -1,40 +1,70 @@
 package accountd
 
 import (
+	"crypto/rsa"
+	"encoding/json"
 	"errors"
-	"strconv"
+	"io/ioutil"
+	"net/http"
 
-	"gopkg.in/jmcvetta/napping.v3"
+	jwt "gopkg.in/dgrijalva/jwt-go.v3"
 )
 
 var HOST = "https://accountd.xyz"
 
-func VerifyAuth(code string) (LookupResponse, error) {
-	res := LookupResponse{}
-	r, err := napping.Post(HOST+"/verify/"+code, nil, &res, nil)
-	if err == nil {
-		if r.Status() > 299 {
-			err = errors.New("uud returned error: " + strconv.Itoa(r.Status()))
-		}
-		if res.Error != "" {
-			err = errors.New(res.Error)
-		}
+func NewClient() Client {
+	resp, err := http.Get(HOST + "/public-key")
+	if err != nil {
+		panic(err)
 	}
-	return res, err
+	defer resp.Body.Close()
+
+	keyb, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	pubkey, err := jwt.ParseRSAPublicKeyFromPEM(keyb)
+	if err != nil {
+		panic(err)
+	}
+
+	return Client{
+		PubKey: pubkey,
+	}
 }
 
-func LookupUser(name string) (LookupResponse, error) {
-	res := LookupResponse{}
-	r, err := napping.Get(HOST+"/lookup/"+name, nil, &res, nil)
-	if err == nil {
-		if r.Status() > 299 {
-			err = errors.New("uud returned error: " + strconv.Itoa(r.Status()))
-		}
-		if res.Error != "" {
-			err = errors.New(res.Error)
-		}
+type Client struct {
+	PubKey *rsa.PublicKey
+}
+
+func (c Client) VerifyAuth(tokenstr string) (user string, err error) {
+	token, err := jwt.Parse(tokenstr, func(*jwt.Token) (interface{}, error) {
+		return c.PubKey, nil
+	})
+	if err != nil {
+		return "", err
 	}
-	return res, err
+	iuser, ok := token.Claims.(jwt.MapClaims)["user"]
+	if !ok {
+		return "", errors.New("jwt token doesn't have field 'user'.")
+	}
+
+	user, ok = iuser.(string)
+	if !ok || user == "" {
+		return "", errors.New("'user' field doesn't contain a string.")
+	}
+	return
+}
+
+func Lookup(name string) (res LookupResponse, err error) {
+	resp, err := http.Get(HOST + "/lookup/" + name)
+	if err != nil {
+		return
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	return
 }
 
 type LookupResponse struct {
