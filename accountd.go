34b08df85dcd3a232b47bd938eac7080e8b4e8c0
fiatjaf <fiatjaf@gmail.com>
2019-01-02 16:40:28 +0000

    adapt to new accountd all-included jwts.

--- accountd.go
+++ accountd.go
@@ -2,7 +2,6 @@ package accountd
 
 import (
 	"crypto/rsa"
-	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"net/http"
@@ -38,45 +37,29 @@ type Client struct {
 	PubKey *rsa.PublicKey
 }
 
-func (c Client) VerifyAuth(tokenstr string) (user string, err error) {
-	token, err := jwt.Parse(tokenstr, func(*jwt.Token) (interface{}, error) {
-		return c.PubKey, nil
-	})
-	if err != nil {
-		return "", err
-	}
-	iuser, ok := token.Claims.(jwt.MapClaims)["user"]
-	if !ok {
-		return "", errors.New("jwt token doesn't have field 'user'.")
-	}
-
-	user, ok = iuser.(string)
-	if !ok || user == "" {
-		return "", errors.New("'user' field doesn't contain a string.")
-	}
-	return
-}
-
-func Lookup(name string) (res LookupResponse, err error) {
-	resp, err := http.Get(HOST + "/lookup/" + name)
+func (c Client) VerifyAuth(tokenstr string) (data TokenData, err error) {
+	token, err := jwt.ParseWithClaims(
+		tokenstr, &data, func(*jwt.Token) (interface{}, error) {
+			return c.PubKey, nil
+		},
+	)
 	if err != nil {
 		return
 	}
 
-	err = json.NewDecoder(resp.Body).Decode(&res)
-	return
+	if claims, ok := token.Claims.(*TokenData); ok && token.Valid {
+		data = *claims
+		return
+	} else {
+		return data, errors.New("jwt token invalid fields.")
+	}
 }
 
-type LookupResponse struct {
-	// this will be returned only if the account
-	// used in the lookup is not known
-	Type string `json:"type"`
-
-	Id       string `json:"id"`
-	Accounts []struct {
-		Type    string `json:"type"`
-		Account string `json:"account"`
-	} `json:"accounts"`
-
-	Error string `json:"error"`
+type TokenData struct {
+	User struct {
+		Name string
+		Id   int
+	}
+	Accounts []string
+	jwt.StandardClaims
 }
